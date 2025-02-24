87c9f36ac7b62ffa3d326630182cd4478aa30505
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-02-24 11:27:57 +0100

    First tests and SmartDoorLockImpl

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,22 @@
 package tdd;
 
+import impl.SmartDoorLockImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    public static int PIN = 1234;
+    public SmartDoorLock smartDoorLock = new SmartDoorLockImpl();
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testLocked() {
+        assertTrue(smartDoorLock.isLocked());
+    }
+
+    @Test
+    void testUnlocked() {
+        smartDoorLock.unlock(PIN);
+        assertFalse(smartDoorLock.isLocked());
     }
 }
